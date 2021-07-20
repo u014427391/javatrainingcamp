616610636b517306a13be11fa438a16510d3e7ba
nicky <362330721@qq.com>
2021-07-20 15:26:52 +0800

    提交工程代码

---
+++ java-core-datastructure/src/main/java/com/example/datastructure/JavaCoreDatastructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.datastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoreDatastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCoreDatastructureApplication.class, args);
+    }
+
+}
