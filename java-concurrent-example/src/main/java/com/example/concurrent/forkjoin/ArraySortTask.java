be972569ed81e66d0d872627fcdc3b9b90cd6aa8
Nicky.Ma <362330721@qq.com>
2021-10-15 17:18:45 +0800

    ForkJoin例子

--- java-concurrent-example/src/main/java/com/example/concurrent/forkjoin/ArraySortTask.java
+++ java-concurrent-example/src/main/java/com/example/concurrent/forkjoin/ArraySortTask.java
@@ -1,8 +1,14 @@
 package com.example.concurrent.forkjoin;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
 /**
  * <pre>
- *
+ *      大数组排序
  * </pre>
  * <p>
  * <pre>
@@ -11,5 +17,59 @@ package com.example.concurrent.forkjoin;
  *    修改后版本:     修改人：  修改日期: 2021/10/12 17:04  修改内容:
  * </pre>
  */
-public class ArraySortTask {
+public class ArraySortTask extends RecursiveAction{
+
+    final long[] array; final int lo, hi;
+    ArraySortTask(long[] array, int lo, int hi) {
+        this.array = array; this.lo = lo; this.hi = hi;
+    }
+    ArraySortTask(long[] array) { this(array, 0, array.length); }
+
+    @Override
+    protected void compute() {
+        if (hi - lo < THRESHOLD)
+            // 少于阀值,使用Arrays.sort 快排
+            sortSequentially(lo, hi);
+        else {
+            /* 归并排序 */
+            // 取中间值
+            int mid = (lo + hi) >>> 1;
+            // 拆分任务
+            invokeAll(new ArraySortTask(array, lo, mid),
+                    new ArraySortTask(array, mid, hi));
+            // 归并结果
+            merge(lo, mid, hi);
+        }
+    }
+
+    // implementation details follow:
+    static final int THRESHOLD = 1000;
+    void sortSequentially(int lo, int hi) {
+        Arrays.sort(array, lo, hi);
+    }
+
+    void merge(int lo, int mid, int hi) {
+        long[] buf = Arrays.copyOfRange(array, lo, mid);
+        for (int i = 0, j = lo, k = mid; i < buf.length; j++)
+            array[j] = (k == hi || buf[i] < array[k]) ?
+                    buf[i++] : array[k++];
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int size = 10_000;
+        long[] array = new long[size];
+        Random random = new Random();
+        for (int i = 0; i< size; i++) {
+            array[i] = random.nextInt();
+        }
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        ArraySortTask task = new ArraySortTask(array , 0 , size);
+        forkJoinPool.submit(task);
+        task.get();
+
+        for (long a : array) {
+            System.out.println(a);
+        }
+    }
 }
--- java-concurrent-example/src/main/java/com/example/concurrent/forkjoin/Fibonacci.java
+++ java-concurrent-example/src/main/java/com/example/concurrent/forkjoin/Fibonacci.java
@@ -1,8 +1,13 @@
 package com.example.concurrent.forkjoin;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
 /**
  * <pre>
- *
+ *      斐波那契数列
  * </pre>
  * <p>
  * <pre>
@@ -11,5 +16,30 @@ package com.example.concurrent.forkjoin;
  *    修改后版本:     修改人：  修改日期: 2021/10/12 16:22  修改内容:
  * </pre>
  */
-public class Fibonacci {
+public class Fibonacci extends RecursiveTask<Integer>{
+
+    private int n;
+
+    public Fibonacci(int n) {
+        this.n = n;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (n <= 1)
+            return n;
+        Fibonacci f1 = new Fibonacci(n - 1);
+        f1.fork();
+        Fibonacci f2 = new Fibonacci(n - 2);
+        f2.fork();
+        return f1.join() + f2.join();
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ForkJoinPool pool = new ForkJoinPool();
+        for (int i = 0; i< 10; i++) {
+            ForkJoinTask task = pool.submit(new Fibonacci(i));
+            System.out.println(task.get());
+        }
+    }
 }
