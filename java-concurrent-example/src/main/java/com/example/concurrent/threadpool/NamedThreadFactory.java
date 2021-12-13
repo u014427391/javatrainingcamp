f0c3364cf4792fe111641df2d5dc38f9eb357100
nicky <362330721@qq.com>
2021-12-13 15:26:33 +0800

    自定义线程池工厂类

---
+++ java-concurrent-example/src/main/java/com/example/concurrent/threadpool/NamedThreadFactory.java
@@ -0,0 +1,41 @@
+package com.example.concurrent.threadpool;
+
+
+import org.springframework.util.StringUtils;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NamedThreadFactory implements ThreadFactory {
+    private static final AtomicInteger poolNumber = new AtomicInteger(1);
+    /** 线程组 **/
+    private final ThreadGroup group;
+    /** 原子类保证计数线程安全 **/
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    /** 命名前缀 **/
+    private final String namePrefix;
+
+    NamedThreadFactory(String threadFactoryName) {
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() :
+                Thread.currentThread().getThreadGroup();
+        threadFactoryName = StringUtils.isEmpty(threadFactoryName)
+                ? "pool-" +poolNumber.getAndIncrement()
+                : threadFactoryName;
+        namePrefix = threadFactoryName + "-thread-";
+    }
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r,
+                namePrefix + threadNumber.getAndIncrement(),
+                0);
+        // 守护线程
+        if (t.isDaemon())
+            t.setDaemon(false);
+        // 优先级
+        if (t.getPriority() != Thread.NORM_PRIORITY)
+            // 标准优先级
+            t.setPriority(Thread.NORM_PRIORITY);
+        return t;
+    }
+}
